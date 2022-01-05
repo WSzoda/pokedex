d606d0d278f6393b5b3c739339662f09cbb2e828
Wojciech Szoda <wojciechszoda@icloud.com>
2022-01-05 17:42:33 +0100

    Little cosmetic changes

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,36 +1,37 @@
 import { useState, useEffect } from "react"
 
 import ListButton from "./ListButton";
+import "./PokemonList.css"
 
 function PokemonList(props) {
     const [pokemons, setPokemons] = useState([]);
 
 
     useEffect(() => {
-        downlaodPokemonList();
+        downloadPokemonList();
     }, [])
 
-    async function downlaodPokemonList(){
+    async function downloadPokemonList(){
         const res = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=1200');
         const json = await res.json();
         setPokemons(json.results);
+    }
+
+    function renderListButton(pokemon){
+        return <ListButton pokemon={pokemon} setSelectedPokemon={props.setSelectedPokemon}/>;
     }
 
     function renderPokemonList(){
         var poke;
         if(props.searchWord === ""){
             poke = pokemons.map((pokemon) => {
-                return(
-                    <ListButton pokemon={pokemon} setSelectedPokemon={props.setSelectedPokemon}/>
-                );
+                return renderListButton(pokemon);
             })
         } else{
             poke = pokemons.map((pokemon) => {
                 if(pokemon.name.includes(props.searchWord)){
-                return(
-                    <ListButton pokemon={pokemon} setSelectedPokemon={props.setSelectedPokemon}/>
-                );
-            }
+                    return renderListButton(pokemon);
+                }
             return null;
             })
         }
@@ -40,7 +41,7 @@ function PokemonList(props) {
 
     return(
         <nav>
-            <ul>
+            <ul className="list">
                 {renderPokemonList()}
             </ul>
         </nav>
